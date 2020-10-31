a958891c6781c88ceedf9412a07261ce23ecf442
Kriti Kalpa <krtklpsh@gmail.com>
2020-10-31 07:56:38 +0530

    [Kriti] Add. Total Fare for multiple rides

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -19,5 +19,18 @@ namespace CabInvoiceGeneratorTest
 
             Assert.AreEqual(expected, fare);
         }
+
+
+        [Test]
+        public void GivenMultipleRidesShouldReturnInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+
+            Assert.AreEqual(expectedSummary.totalFare, summary.totalFare);
+        }
     }
 }
